6a546c733bc3882eb7aecec58db9d1b6113c1fe6
saraahamrr <saraahamrr98@gmail.com>
2025-02-02 21:59:33 +0200

    connectdb_schema_

---
+++ UserProfile/backend/connections/conn.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+//const mongoose = require('mongoose');
+import dotenv from 'dotenv';
+dotenv.config();
+const conn = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Connected to database');
+    }
+    catch(err){
+        console.log('this is an error consoledlog',err);
+    }
+}
+
+conn();
