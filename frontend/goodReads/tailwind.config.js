0b94e4c57fd1789c8a21b7ec7b9b5b76ef4568ac
saraahamrr <saraahamrr98@gmail.com>
2025-02-04 14:08:59 +0200

    commit

--- frontend/goodReads/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // تأكد إن المسارات صحيحة
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
