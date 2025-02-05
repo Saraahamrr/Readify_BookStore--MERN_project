63ea02075b76a61b6add37bf69329df77f30c586
Menna Reda <menna.r712@gmail.com>
2025-02-05 13:46:02 +0200

    book patch edit

--- backend/routes/books.route.js
+++ backend/routes/books.route.js
@@ -42,7 +42,36 @@ router
 router
   .route("/:bookId")
   .get(bookController.getBook)
-  .patch(bookController.updateBook)
+  .patch(
+    [
+    //   body("id").notEmpty().withMessage("id is required"),
+      body("title")
+        .notEmpty()
+        .withMessage("Title is required")
+        .isLength({ min: 2 })
+        .withMessage("Title must be at least 2 characters"),
+      body("authors")
+        .notEmpty()
+        .withMessage("Authors are required")
+        .isArray()
+        .withMessage("Authors must be an array"),
+      body("description").notEmpty().withMessage("Description is required"),
+      body("coverImage").notEmpty().withMessage("Cover image is required"),
+      body("fullContent").notEmpty().withMessage("Full content is required"),
+      body("publisher").notEmpty().withMessage("Publisher is required"),
+      body("publishedDate")
+        .notEmpty()
+        .withMessage("Published date is required")
+        .isISO8601()
+        .withMessage("Published date must be a valid date"),
+      body("categories")
+        .notEmpty()
+        .withMessage("Categories are required")
+        .isArray()
+        .withMessage("Categories must be an array"),
+      body("language").notEmpty().withMessage("Language is required"),
+      body("price").notEmpty().withMessage("Price is required").isFloat({ gt: 0 }).withMessage("Price must be a positive number"),
+    ],bookController.updateBook)
   .delete(bookController.deleteBook);
 
 router.post(
