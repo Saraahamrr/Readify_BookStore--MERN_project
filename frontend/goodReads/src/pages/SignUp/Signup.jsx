b18d366949127ff45ace0cf614fef4dc60db0db4
saraahamrr <saraahamrr98@gmail.com>
2025-02-05 14:55:12 +0200

    commit

--- frontend/goodReads/src/pages/SignUp/Signup.jsx
+++ frontend/goodReads/src/pages/SignUp/Signup.jsx
@@ -3,7 +3,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./Signup.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebookF, faGooglePlusG, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebookF,
+  faGooglePlusG,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Signup() {
   const [formValues, setFormValues] = useState({
@@ -11,27 +15,33 @@ export default function Signup() {
     email: "",
     username: "",
     password: "",
-    confirmpassword: "",
+    address: "",
   });
-
+  const navigate = useNavigate();
   const [formErrors, setFormErrors] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
 
   const validateForm = (values) => {
     const errors = {};
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-
-    if (!values.name) errors.name = "Name is required";
-    if (!values.email) errors.email = "Email is required";
-    else if (!emailRegex.test(values.email)) errors.email = "Wrong email format";
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
 
     if (!values.username) errors.username = "Username is required";
-    else if (/\s/.test(values.username)) errors.username = "Username must not contain spaces";
+    else if (/\s/.test(values.username))
+      errors.username = "Username must not contain spaces";
+
+    if (!values.email) errors.email = "Email is required";
+    else if (!emailRegex.test(values.email))
+      errors.email = "Wrong email format";
 
     if (!values.password) errors.password = "Password is required";
     else if (!passwordRegex.test(values.password))
-      errors.password = "Password must include uppercase, lowercase, special symbol, numbers";
+      errors.password =
+        "Password must include uppercase, lowercase, special symbol, numbers";
+    if (!values.address) errors.address = "Address is required";
+    else if (values.address.length < 10)
+      errors.address = "Address must be at least 10 characters";
 
     return errors;
   };
@@ -42,14 +52,19 @@ export default function Signup() {
     setFormErrors(errors);
     if (Object.keys(errors).length === 0) {
       setIsSubmitted(true);
+
       alert("Data submitted successfully");
       setFormValues({
         name: "",
         email: "",
         username: "",
         password: "",
-        confirmpassword: "",
+        address: "",
       });
+      console.log(formValues);
+      const response = await axios.post("http://localhost:3000/api/sign-up",formValues);
+      console.log(response);
+      navigate("/sign-in");
     } else {
       setIsSubmitted(false);
     }
@@ -68,81 +83,151 @@ export default function Signup() {
     const handleSignUpClick = () => main.classList.add("right-panel-active");
     const handleSignInClick = () => main.classList.remove("right-panel-active");
 
-    signUpButtons.forEach((btn) => btn.addEventListener("click", handleSignUpClick));
-    signInButtons.forEach((btn) => btn.addEventListener("click", handleSignInClick));
+    signUpButtons.forEach((btn) =>
+      btn.addEventListener("click", handleSignUpClick)
+    );
+    signInButtons.forEach((btn) =>
+      btn.addEventListener("click", handleSignInClick)
+    );
 
     return () => {
-      signUpButtons.forEach((btn) => btn.removeEventListener("click", handleSignUpClick));
-      signInButtons.forEach((btn) => btn.removeEventListener("click", handleSignInClick));
+      signUpButtons.forEach((btn) =>
+        btn.removeEventListener("click", handleSignUpClick)
+      );
+      signInButtons.forEach((btn) =>
+        btn.removeEventListener("click", handleSignInClick)
+      );
     };
   }, []);
 
   return (
     <>
-    <div className="container-fluid">
-    <div className="custom-form-container">
-    <div className="containerr" id="main">
-        <div className="form-container">
-          <div className="sign-up">
-            <form onSubmit={handleSubmit}>
-              <h1>Create Account</h1>
-              <div className="social-container">
-                <a href="#" className="social"><FontAwesomeIcon icon={faFacebookF} /></a>
-                <a href="#" className="social"><FontAwesomeIcon icon={faGooglePlusG} /></a>
-                <a href="#" className="social"><FontAwesomeIcon icon={faLinkedinIn} /></a>
-              </div>
-              <p>or use your email for registration</p>
-              <input type="text" name="name" placeholder="Name" value={formValues.name} onChange={handleChange} required />
-              {formErrors.name && <p className="error">{formErrors.name}</p>}
-              <input type="email" name="email" placeholder="Email" value={formValues.email} onChange={handleChange} required />
-              {formErrors.email && <p className="error">{formErrors.email}</p>}
-              <input type="text" name="username" placeholder="Username" value={formValues.username} onChange={handleChange} required />
-              {formErrors.username && <p className="error">{formErrors.username}</p>}
-              <input type="password" name="password" placeholder="Password" value={formValues.password} onChange={handleChange} required />
-              {formErrors.password && <p className="error">{formErrors.password}</p>}
-              <button className="signUp" type="submit">Sign Up</button>
-            </form>
-          </div>
+      <div className="container-fluid">
+        <div className="custom-form-container">
+          <div className="containerr" id="main">
+            <div className="form-container">
+              <div className="sign-up">
+                <form onSubmit={handleSubmit}>
+                  <h1>Create Account</h1>
+                  <div className="social-container">
+                    <a href="#" className="social">
+                      <FontAwesomeIcon icon={faFacebookF} />
+                    </a>
+                    <a href="#" className="social">
+                      <FontAwesomeIcon icon={faGooglePlusG} />
+                    </a>
+                    <a href="#" className="social">
+                      <FontAwesomeIcon icon={faLinkedinIn} />
+                    </a>
+                  </div>
+                  <input
+                    type="text"
+                    name="username"
+                    placeholder="Username"
+                    value={formValues.username}
+                    onChange={handleChange}
+                    required
+                  />
+                  {formErrors.username && (
+                    <p className="error">{formErrors.username}</p>
+                  )}
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={formValues.email}
+                    onChange={handleChange}
+                    required
+                  />
+                  {formErrors.email && (
+                    <p className="error">{formErrors.email}</p>
+                  )}
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={formValues.password}
+                    onChange={handleChange}
+                    required
+                  />
+                  {formErrors.password && (
+                    <p className="error">{formErrors.password}</p>
+                  )}
 
-          <div className="sign-in">
-            <form  action="#">
-              <h1>Sign in</h1>
-              <div className="social-container">
-                <a href="#" className="social"><FontAwesomeIcon icon={faFacebookF} /></a>
-                <a href="#" className="social"><FontAwesomeIcon icon={faGooglePlusG} /></a>
-                <a href="#" className="social"><FontAwesomeIcon icon={faLinkedinIn} /></a>
+                  <input
+                    type="text"
+                    name="address"
+                    placeholder="address"
+                    value={formValues.address}
+                    onChange={handleChange}
+                    required
+                  />
+                  {formErrors.address && (
+                    <p className="error">{formErrors.address}</p>
+                  )}
+                  <button className="signUp" type="submit">
+                    Sign Up
+                  </button>
+                </form>
               </div>
-              <p>or use your email for login</p>
-              <input type="email" name="email" placeholder="Email" required />
-              <input type="password" name="password" placeholder="Password" required />
-              <a href="#">Forget Password?</a>
-              <button className="signIn" type="submit">Sign In</button>
-            </form>
-          </div>
-        </div>
 
-        <div className="overlay-container">
-          <div className="overlay">
-            <div className="overlay-left">
-              <h1>Welcome Back!</h1>
-              <p>To keep connected with us please login with your personal info</p>
-              <button className="signIn">Sign In</button>
+              <div className="sign-in">
+                <form action="#">
+                  <h1>Sign in</h1>
+                  <div className="social-container">
+                    <a href="#" className="social">
+                      <FontAwesomeIcon icon={faFacebookF} />
+                    </a>
+                    <a href="#" className="social">
+                      <FontAwesomeIcon icon={faGooglePlusG} />
+                    </a>
+                    <a href="#" className="social">
+                      <FontAwesomeIcon icon={faLinkedinIn} />
+                    </a>
+                  </div>
+                  <p>or use your email for login</p>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    required
+                  />
+                  <a href="#">Forget Password?</a>
+                  <button className="signIn" type="submit">
+                    Sign In
+                  </button>
+                </form>
+              </div>
             </div>
-            <div className="overlay-right">
-              <h1>Hello, Reader</h1>
-              <p>Enter your personal details and start the journey with us</p>
-              <button className="signUp">Sign Up</button>
+
+            <div className="overlay-container">
+              <div className="overlay">
+                <div className="overlay-left">
+                  <h1>Welcome Back!</h1>
+                  <p>
+                    To keep connected with us please login with your personal
+                    info
+                  </p>
+                  <button className="signIn">Sign In</button>
+                </div>
+                <div className="overlay-right">
+                  <h1>Hello, Reader</h1>
+                  <p>
+                    Enter your personal details and start the journey with us
+                  </p>
+                  <button className="signUp">Sign Up</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-
-
-    </div>
-
-    </div>
-    
-      
     </>
   );
 }
