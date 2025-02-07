de2d5430360b44448fd6b73e0fdb6003949b01c7
saraahamrr <saraahamrr98@gmail.com>
2025-02-07 22:17:08 +0200

    m

--- frontend/goodReads/src/App.jsx
+++ frontend/goodReads/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header/Header";
+import Header from "./components/Header/header";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
 import { BooksProvider } from "./context/books";
