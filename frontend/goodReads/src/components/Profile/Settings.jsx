1b6d03c0d70019bda8049131208463942b38431f
Menna Reda <menna.r712@gmail.com>
2025-02-09 21:33:56 +0200

    settings

--- frontend/goodReads/src/components/Profile/Settings.jsx
+++ frontend/goodReads/src/components/Profile/Settings.jsx
@@ -1,4 +1,12 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 export default function Settings(){
+    useEffect(()=>{
+        const fetch = async () =>{
+            const response = await axios.get("http://localhost:3000");
+        }
+    },[])
     return(
         <p>Settings</p>
     );
