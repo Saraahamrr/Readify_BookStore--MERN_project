daacebba1bf93d410ee22890fbbb5acca8b0fb33
Menna Reda <menna.r712@gmail.com>
2025-02-09 17:13:00 +0200

    autherize components in front

---
+++ frontend/goodReads/src/components/Profile/BookManagement/BookManagement.jsx
@@ -0,0 +1,120 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan,faPen } from "@fortawesome/free-solid-svg-icons";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Bounce } from "react-toastify";
+import BooksContext from "../../../context/books";
+import Loader from "../../Loader/Loader";
+import axios from "axios";
+import "./BookManagement.css";
+
+
+ const deleteBook = async (bookId, setBooks) => {
+  const confirmation = confirm("Are you sure you want to delete this book?");
+  if (!confirmation) return;
+  try {
+    axios.defaults.withCredentials = true;
+    const response = await axios.delete(
+      `http://localhost:3000/api/books/${bookId}`
+    );
+    console.log(response); // Debugging
+    if (response.status === 200) {
+      setBooks((prevBooks) => prevBooks.filter((book) => book._id !== bookId));
+      toast.success(response.data.msg, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+                transition: Bounce,
+              });
+    }
+  } catch (error) {
+    console.error("Error deleting book:", error);
+    toast.error(error.response.data.msg, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+                transition: Bounce,
+              });
+  }
+};
+
+
+export default function BookManagement() {
+  const { books, setBooks, loading } = useContext(BooksContext);
+  const navigate = useNavigate();
+  if (loading) return <Loader />;
+  const updateBook = (bookId)=>{
+    navigate(`/update-book/${bookId}`)
+  }
+  return (
+    <div className="p-5 d-flex flex-column align-items-center">
+   
+      <table className="m-3 table table-striped table-hover table-light ">
+        <thead className="thead-dark" style={{ position: "sticky", top: 0 }}>
+          <tr>
+            <th scope="col">id</th>
+            <th scope="col">title</th>
+            <th scope="col">authors</th>
+            <th scope="col">categories</th>
+            <th scope="col">description</th>
+            <th scope="col">Average rating</th>
+            <th scope="col">language</th>
+            <th scope="col">price</th>
+            <th scope="col">edit/delete book</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.length > 0 ? (
+            books.map((book) => (
+              <tr scope="row" key={book._id}>
+                <td>{book.id}</td>
+                <td>{book.title}</td>
+                <td>{book.authors?.map((author) => author.name).join(", ")}</td>
+                <td>
+                  {book.categories?.map((category) => category.name).join(", ")}
+                </td>
+                <td><div className="content-wrapper">{book.description}</div></td>
+                <td>{book.averageRating}</td>
+                <td>{book.language}</td>
+                <td>{book.price}</td>
+                <td>
+                <button
+                    className="editButton"
+                    onClick={() => updateBook(book._id)}
+                  >
+                    <FontAwesomeIcon icon={faPen} />
+                  </button>
+                  <button
+                    className="trashButton"
+                    onClick={() => deleteBook(book._id, setBooks)}
+                  >
+                    <FontAwesomeIcon icon={faTrashCan} />
+                  </button>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <Loader />
+          )}
+        </tbody>
+      </table>
+      <div className="align-self-end">
+        <button type="button" style={{ backgroundColor: "#fbb02d", border:0, color:"white", fontWeight:"bold"}} className="custom-btn sign-btn"
+        >+ New book</button>
+      </div>
+    </div>
+  );
+}
+
